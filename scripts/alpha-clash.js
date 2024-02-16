2c122e49e80f1c4fa8b0c7b31028dc7fc84de26f
sabikun628 <sabikun0628@gmail.com>
2024-02-17 03:42:48 +0600

    add some alpha clash

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,3 +1,35 @@
-function play(){
-    console.log('play start now');
+// function play() {
+
+//     const homeSection = document.getElementById('home');
+//     homeSection.classList.add('hidden')
+
+
+
+//     const playGround = document.getElementById('play-ground')
+//     playGround.classList.remove('hidden')
+//     console.log(playGround.classList);
+
+// }
+
+function continueGame() {
+    const alphabet = getARandomAlphabet();
+    console.log(alphabet);
+
+
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+   
+    currentAlphabetElement.innerText = alphabet;
+
+    setBackgroundColor(alphabet);
+}
+
+
+
+
+
+
+function play() {
+    hideElementById('home');
+    showElementById('play-ground');
+    continueGame('')
 }
