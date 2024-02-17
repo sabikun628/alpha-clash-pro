8063b93d6b7df8afc3f7bb19b2905b9ef82dcd50
sabikun628 <sabikun0628@gmail.com>
2024-02-17 20:05:12 +0600

    added some js

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -11,13 +11,56 @@
 
 // }
 
+
+function handleKeyboardKeyUpEvent(event) {
+    const playerPress = event.key
+
+
+    const currentAlphabetElement = document.getElementById('current-alphabet')
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+    console.log(playerPress, expectedAlphabet);
+
+
+    if (playerPress === expectedAlphabet) {
+        console.log('you get a point');
+
+
+
+
+        const currentScoreElement = document.getElementById('current-score')
+        const currentScoreText = currentScoreElement.innerText;
+        const currentScore = parseInt(currentScoreText)
+
+        const newScore = currentScore + 1
+        currentScoreElement.innerText = newScore;
+
+        console.log('you have pressed correctly', expectedAlphabet);
+        removeBackgroundColor(expectedAlphabet);
+        continueGame();
+    } else {
+        const currentLifeElement = document.getElementById('current-life');
+        const currentLifeText = currentLifeElement.innerText;
+        const currentLife = parseInt(currentLifeText)
+
+        const newLife = currentLife - 1;
+        currentLifeElement.innerText = newLife;
+
+        console.log('you missed. lose a lifeline. try again ');
+
+    }
+}
+
+document.addEventListener('keyup', handleKeyboardKeyUpEvent)
+
+
 function continueGame() {
     const alphabet = getARandomAlphabet();
     console.log(alphabet);
 
 
     const currentAlphabetElement = document.getElementById('current-alphabet');
-   
+
     currentAlphabetElement.innerText = alphabet;
 
     setBackgroundColor(alphabet);
--- scripts/utlity.js
+++ scripts/utlity.js
@@ -12,7 +12,10 @@ function  setBackgroundColor(elementId) {
     element.classList.add('bg-orange-400');
 }
 
-
+function  removeBackgroundColor(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-orange-400');
+}
 
 
 function getARandomAlphabet() {
